71ea597793ee205269ffdac0bfea4410cfb82730
Fabian Schultz <fschultz02@gmail.com>
2015-11-15 16:44:18 +0100

    Saving images every 10 seconds, showing them on /.

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 ========================================================================== */
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app),
+    http = require('http'),
+    https = require('https'),
+    server = http.createServer(app),
     jade = require('jade'),
+    cron = require('cron'),
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url'),
     reddit = require('redwrap');
 
 
@@ -11,51 +17,109 @@ var express = require('express'),
 ========================================================================== */
 app.use('/', express.static(__dirname + '/public'));
 
+
+
 /* JADE
 ========================================================================== */
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.get('/r', function (req, res) {
+
+
+/* ACTION
+========================================================================== */
+var getImage = cron.job("*/10 * * * * *", function(){
   reddit.r('random', function(err, data, response){
-    if (err) {
-      res.send(err);
-      console.log(err);
-    }
+    if (err) throw err;
 
     for (var i = 0; i < data.data.children.length; i++) {
-      console.log(i);
       var gif = data.data.children[i].data.url;
       var preview = data.data.children[i].data.preview;
       var r = data.data.children[0].data.subreddit;
       
       if (gif.endsWith('.gif')) {
-        res.render('index', { data: gif, r: r });
+        downloadImage(gif, r);
         break;
       } else if (typeof preview !== 'undefined') {
-        res.render('index', { data: data.data.children[i].data.preview.images[0].source.url, r: r });
+        downloadImage(preview.images[0].source.url, r);
         break;
       } else if (i == data.data.children.length - 1) {
-        res.send('No images found at r/' + r + '.');
+        console.log('No images found at r/' + r + '.');
         break;
       }
     };
   });
 });
 
-app.get('/json', function (req, res) {
-  reddit.r('funny', function(err, data, response){
-    res.send(data.data.children[0].data.subreddit);
-  });
+
+
+/* API
+========================================================================== */
+app.get('/api/start', function (req, res) {
+  getImage.start();
+  res.send('<h1>API started.</h1>');
 });
 
-app.get('/api', function (req, res) {
-  res.send('wokdeod');
+app.get('/api/end', function (req, res) {
+  getImage.end();
+  res.send('<h1>API ended.</h1>');
 });
+
+app.get('/', function (req, res) {
+  var images = getFiles('public/images');
+  res.render('index', { images: images });
+});
+
+
+
+/* FUNCTIONS
+========================================================================== */
+function downloadImage(img_url, subreddit) {
+  if (stringStartsWith(img_url, 'https')) {
+    protocol = https;
+  } else {
+    protocol = http;
+  }
+  var request = protocol.get(img_url, function(res){
+      var imagedata = '';
+      res.setEncoding('binary');
+
+      res.on('data', function(chunk){
+          imagedata += chunk;
+      })
+
+      res.on('end', function(){
+          fs.writeFile('public/images/' + subreddit + path.extname(url.parse(img_url).pathname), imagedata, 'binary', function(err){
+              if (err) throw err;
+              console.log('File saved.');
+          });
+      });
+  });
+};
+
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push({file: path.basename(name), sub: path.basename(name, path.extname(name)) });
+        }
+    }
+    return files_;
+}
+
+function stringStartsWith (string, prefix) {
+    return string.slice(0, prefix.length) == prefix;
+}
 
 String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
+/* SERVER
+========================================================================== */
 console.log('Listening on port http://localhost:8000');
 server.listen(8000, '0.0.0.0');
